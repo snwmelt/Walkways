b4ee29478086fcdbefa43828bade701a2164efe3
snwmelt <ekitai.k@gmail.com>
2018-01-12 14:14:49 +0000

    Initial commit

---
+++ Walkways/MVVM/AObservable.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Walkways.MVVM
+{
+    /// <summary>
+    /// An Abstract Class For Use In Creating Observable Objects.
+    /// </summary>
+    public abstract class AObservable : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Assigns Value To Referenced Property And Raises The INotifyPropertyChanged Property Changed Event.
+        /// </summary>
+        /// <typeparam name="T">Type Specifier.</typeparam>
+        /// <param name="Property">Reference To Property To Be Assigned The Value.</param>
+        /// <param name="PropertyName">Property Name/Title To Be Used When Raising Property Changed Event.</param>
+        /// <param name="Value">Value To Be Assigned To The Referenced Property.</param>
+        protected void AssignPropertyValue<T>( ref T Property, T Value, [CallerMemberName] String PropertyName = "" )
+        {
+            Property = Value;
+
+            PropertyChanged?.Invoke( this, new PropertyChangedEventArgs( PropertyName ) );
+        }
+
+        /// <summary>
+        /// Raises The INotifyPropertyChanged Property Changed Event.
+        /// If No Parameter Is Passed The Name Of The Calling Function Will Be Infered And Used As The Value Of PropertyName.
+        /// </summary>
+        /// <param name="PropertyName">Infered / Explicit Property Name.</param>
+        protected void NotifyPropertyChanged( [ CallerMemberName ] String PropertyName = "" )
+        {
+            PropertyChanged?.Invoke( this, new PropertyChangedEventArgs( PropertyName ) );
+        }
+
+        /// <summary>
+        /// INotifyPropertyChanged Property Changed Event Handler.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ Walkways/MVVM/CommandRelay.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Walkways.MVVM
+{
+    /// <summary>
+    /// An Implementation of The ICommand Interface That Supports Actions Following The Signiture A<T>(T Value).
+    /// </summary>
+    /// <typeparam name="T">Tupe Specifier.</typeparam>
+    public class CommandRelay<T> : ICommand
+    {
+        #region Private Variables
+
+        private readonly Action< T >    _Action;
+        private readonly Predicate< T > _Predicate;
+
+        #endregion
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="Action">Action to Be Executed.</param>
+        /// <param name="Predicate">Predicate to Determine If The Action Parameter Can Be Executed.</param>
+        public CommandRelay( Action< T > Action, Predicate< T > Predicate = null )
+        {
+            
+
+            _Action    = Action;
+            _Predicate = Predicate;
+        }
+
+        /// <summary>
+        /// Action To Be Invoked.
+        /// </summary>
+        public Action< T > Action
+        {
+            get
+            {
+                return _Action;
+            }
+        }
+
+        /// <summary>
+        /// Invokes The Predicate In Order To Determine The Executable Status Of The Action,
+        /// If No Predicate Is Provided The Result Is Always True.
+        /// </summary>
+        /// <param name="Parameter">Parameter For Predicate Invocation.</param>
+        /// <returns>True If Action Is Currently Executable.</returns>
+        public bool CanExecute( T Parameter )
+        {
+            if ( Predicate == null )
+            {
+                return true;
+            }
+
+            return Predicate( Parameter );
+        }
+
+        /// <summary>
+        /// Invokes The Predicate In Order To Determine The Executable Status Of The Action,
+        /// If No Predicate Is Provided The Result Is Always True.
+        /// </summary>
+        /// <param name="Parameter">Parameter For Predicate Invocation.</param>
+        /// <returns>True If Action Is Currently Executable.</returns>
+        public bool CanExecute( Object Parameter )
+        {
+            return CanExecute( ( T )Parameter );
+        }
+
+        /// <summary>
+        /// Utalises COmmandManager To Poll For Changes That Might Affect Weather The Action Can Be Executed.
+        /// </summary>
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+            }
+
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+
+        /// <summary>
+        /// Invokes The Action With A Given Parameter.
+        /// </summary>
+        /// <param name="Parameter">Parameter For Action Invocation.</param>
+        public void Execute( T Parameter )
+        {
+            Action.Invoke( Parameter );
+        }
+
+        /// <summary>
+        /// Invokes The Action With A Given Parameter.
+        /// </summary>
+        /// <param name="Parameter">Parameter For Action Invocation.</param>
+        public void Execute( Object Parameter )
+        {
+            Execute( ( T )Parameter );
+        }
+
+        /// <summary>
+        /// Predicate Used For Determining If The Action Can Be Currently Invoked.
+        /// </summary>
+        public Predicate<T> Predicate
+        {
+            get
+            {
+                return _Predicate;
+            }
+        }
+    }
+}
